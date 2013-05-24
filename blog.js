a4be120fc4375443f642d676134865e24607f006
sjaakvelthoven <sjaak@velthoven.nl>
2013-05-24 21:37:17 +0200

    fail

--- blog.js
+++ blog.js
@@ -31,19 +31,18 @@ function onSearchResponse(response) {
     showResponse(response);
 }
 
+//soundcloud API
 
-//the soundcloud API thing
-SC.initialize({
-  client_id: 'af74921a176ba7c41d3e7b07f55b663c'
-  redirect_uri: "https://soundcloud.com/connect?state=SoundCloud_Dialog_62975&client_id=609ae0b573913db156968e0ec38c1e26&redirect_uri=http%3A%2F%2Fdevelopers.soundcloud.com%2Fcallback.html&response_type=code_and_token&scope=non-expiring&display=popup"
-});
-SC.connect(function() {
-  SC.get('/me', function(me) { 
-    alert('Hello, ' + me.username); 
+<script src="//connect.soundcloud.com/sdk.js"></script>
+
+  SC.initialize({
+    client_id: 'af74921a176ba7c41d3e7b07f55b663c'
+    redirect_uri: "https://soundcloud.com/connect?state=SoundCloud_Dialog_62975&client_id=609ae0b573913db156968e0ec38c1e26&redirect_uri=http%3A%2F%2Fdevelopers.soundcloud.com%2Fcallback.html&response_type=code_and_token&scope=non-expiring&display=popup"
   });
-});
-$(document).ready(function() {
-    SC.get("/tracks/293",function(track){
+  SC.connect(function() {
+    SC.get('/me', function(me) { 
+      alert('Hello, ' + me.username); 
+  });
+  SC.get("/tracks/293",function(track){
       SC.oEmbed(track.permalink_url, document.getElementById('player'));
-    });
-});
+  });
