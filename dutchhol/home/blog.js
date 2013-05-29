410923d168c4c64bc10132738b638d8d34beaff1
sjaakvelthoven <sjaak@velthoven.nl>
2013-05-29 19:04:37 +0200

    lol

---
+++ dutchhol/home/blog.js
@@ -0,0 +1,40 @@
+SC.initialize({
+  client_id:"9f5f0eb2986ff6115aa0ed1f2fae6b2f",
+  redirect_uri:
+SC.initialize({
+  client_id: "9f5f0eb2986ff6115aa0ed1f2fae6b2f",
+  redirect_uri: "https://soundcloud.com/connect?state=SoundCloud_Dialog_62975&client_id=609ae0b573913db156968e0ec38c1e26&redirect_uri=http%3A%2F%2Fdevelopers.soundcloud.com%2Fcallback.html&response_type=code_and_token&scope=non-expiring&display=popup"
+});
+$("document").ready(function(){
+  $("#hide").click(function(){
+      $(this).hide();
+  });
+  $("div#sortable").accordion({
+    collapsible:true
+  });
+  SC.oEmbed("https://soundcloud.com/seetherofficial/fake-it", {color: "ff0066"},  document.getElementById("player"));
+});
+
+
+//learned about API calls... this examle =
+function showResponse(response) {
+    var responseString = JSON.stringify(response, '', 2);
+    document.getElementById('response').innerHTML += responseString;
+}
+function onClientLoad() {
+    gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
+}
+function onYouTubeApiLoad() {
+    gapi.client.setApiKey('AIzaSyCR5In4DZaTP6IEZQ0r1JceuvluJRzQNLE');
+    search();
+}
+function search() {
+    var request = gapi.client.youtube.search.list({
+        part: 'snippet',
+        q: "minecraft"
+    });
+    request.execute(onSearchResponse);
+}
+function onSearchResponse(response) {
+    showResponse(response);
+}
